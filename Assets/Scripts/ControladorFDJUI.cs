c0d00d47d852c1a594619466b74278f92d4874aa
SrSwagsito <andres12felipe34@hotmail.com>
2025-02-25 22:18:37 -0500

    Actualizacion menu principal

---
+++ Assets/Scripts/ControladorFDJUI.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ControladorFDJUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textoPuntaje;
+    private void OnEnable()
+    {
+        ControladorPuntos.Instancia.MejorpuntajeActuallizado += ActualizarTextoPuntaje;
+    }
+
+    public void ActualizarTextoPuntaje(int nuevoPuntaje)
+    {
+        textoPuntaje.text = nuevoPuntaje.ToString(); 
+    }
+}
